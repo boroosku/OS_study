18e820d9334e0b81f8db0aa1d60d4211b3951055
boroosku <boroosku@gmail.com>
2025-01-20 18:52:24 +1000

    fourth commit

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
     cout << "Hello, world!" << endl;
+    std::cout << "Press Enter to exit..." << std::endl;
+    std::cin.get();  // Ожидает ввода
     return 0;
 }
